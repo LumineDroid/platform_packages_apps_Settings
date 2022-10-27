487391025672b9faaf8cdb5517787dd619134188
Hexaboo <hexaboo@outlook.com>
2022-10-27 23:26:52 +0000

    Settings: Apply Extensions to dashboard
    Change-Id: Ib54b9e93269b05e872ebb4e5854e88b69c36c184
    Signed-off-by: Hexaboo <hexaboo@outlook.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -653,4 +653,13 @@ public class Settings extends SettingsActivity {
         }
     }
     public static class SafetyCenterActivity extends SettingsActivity { }
+
+    /** Extensions */
+    public static class ExtensionsActivity extends SettingsActivity {}
+    public static class ExtensionsStatusbarActivity extends SettingsActivity {}
+    public static class ExtensionsQuickSettingsActivity extends SettingsActivity {}
+    public static class ExtensionsButtonActivity extends SettingsActivity {}
+    public static class ExtensionsLockScreenActivity extends SettingsActivity {}
+    public static class ExtensionsAboutActivity extends SettingsActivity {}
+    public static class ExtensionsMiscActivity extends SettingsActivity {}
 }
